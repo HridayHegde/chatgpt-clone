0a12a3bba52a35d7959a4d76e83cdc234aea3601
Hriday Hegde <hridayhegde1999@gmail.com>
2023-03-10 12:08:41 +0530

    Final build

--- components/Login.tsx
+++ components/Login.tsx
@@ -8,7 +8,7 @@ function Login() {
   return (
     <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
         <Image src="https://links.papareact.com/2i6" width={300} height={300} alt="logo" />
-        <button onClick={()=>signIn('google')} className="text-white font-bold text-3xl animate-pulse">Sign In to use ChatGPT</button>
+        <button onClick={()=>signIn('google')} className="text-purple font-bold text-3xl animate-pulse">Sign In to use CloneGPT</button>
     </div>
   )
 }
